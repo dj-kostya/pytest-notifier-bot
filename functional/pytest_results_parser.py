441ed61595873841454fb9bbf6276fd7d1161ebc
SergeyOlymp <serezhasharkov2003@gmail.com>
2022-05-22 18:17:16 +0700

    test

--- functional/pytest_results_parser.py
+++ functional/pytest_results_parser.py
@@ -1,11 +1,12 @@
 import pytest
 import pandas as pd
+from config.env_config import PATH_TO_JSON_REPORT
 
 
 def pytest_results(my_path):
 
     pytest.main(['--json-report', f'--rootdir={my_path}'])
-    data = pd.read_json('../.report.json', lines=True, encoding='utf8')
+    data = pd.read_json(PATH_TO_JSON_REPORT, lines=True, encoding='utf8')
     result = ''
     for test in range(len(data['tests'][0])):
         if data['tests'][0][test]['outcome'] == 'failed':
